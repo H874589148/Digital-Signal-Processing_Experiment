c3e82ee913306f60c6221bd368a00ff9277cc244
Hawkeye <40380233+H874589148@users.noreply.github.com>
2020-02-27 17:04:40 +0800

    第15题

--- 第1章作业/hr15.m
+++ 第1章作业/hr15.m
@@ -1,1 +1,15 @@
-plot(hr_y,hr_t);
+[hr_y]=xlsread('hr15.xlsx','A1:A100');
+[hr_t]=xlsread('hr15.xlsx','B1:B100');
+subplot(311);plot(hr_y,hr_t);
+%hr_t=hr_t-mean(hr_t); %调整hr_t的均值
+n = length(hr_t) ;
+
+rx=xcorr(hr_t,'biased');
+subplot(312) ;
+plot(rx(n:end));%取中点n为起始的后面n个序列
+title('xcorr求自相关') ;
+
+[ACF,lags,bounds] = autocorr(hr_t,n-1) ;
+subplot(313) ;
+plot(lags(1:end),ACF(1:end)) ;
+title('autocorr求自相关') ;
