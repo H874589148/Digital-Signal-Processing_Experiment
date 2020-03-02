330b8168b9386b4d6f6ecb73e1191bcf08ed4776
Hawkeye <40380233+H874589148@users.noreply.github.com>
2020-03-02 20:46:39 +0800

    更正
    题干中给出M=32

--- 第1章作业/hr15.m
+++ 第1章作业/hr15.m
@@ -1,12 +1,14 @@
 [hr_y]=xlsread('hr15.xlsx','A1:A100');
 [hr_t]=xlsread('hr15.xlsx','B1:B100');
 subplot(311);plot(hr_y,hr_t);
+maxlag=32;
 %hr_t=hr_t-mean(hr_t); %调整hr_t的均值
 n = length(hr_t) ;
 
-rx=xcorr(hr_t,'biased');
+rx=xcorr(hr_t,maxlag);%xcorr(,maxlag)xcorr(,'biased')
 subplot(312) ;
-plot(rx(n:end));%取中点n为起始的后面n个序列
+plot(rx(maxlag:end));
+%plot(rx(n:end));%取中点n为起始的后面n个序列
 title('xcorr求自相关') ;
 
 [ACF,lags,bounds] = autocorr(hr_t,n-1) ;
