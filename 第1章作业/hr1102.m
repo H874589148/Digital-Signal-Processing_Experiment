962f24ae16976b19be024256f367064970b38dcb
Hawkeye <40380233+H874589148@users.noreply.github.com>
2020-03-03 22:37:04 +0800

    更正题目
    更正题目数据

--- 第1章作业/hr1102.m
+++ 第1章作业/hr1102.m
@@ -2,16 +2,16 @@ clear all;
 close all;
 A = 2;
 a=0.5;
-f = 50;
-fs = 400; % 采样率设置Hz
+f = 2;
+fs = 16; % 采样率设置Hz
 Ts=1/fs;
 w = 2*pi*f*Ts;
-n =0:300;
+n =0:80;
 y = A.*exp(-a.*Ts.*n).*sin(w.*n);
 subplot(211);stem(n,y,'.');
-fs = 800; % 采样率设置改为 800Hz
+fs = 32; % 采样率设置改为 800Hz
 Ts=1/fs;
 w = 2*pi*f*Ts;
-n =0:300;
+n =0:80;
 y = A.*exp(-a.*Ts.*n).*sin(w.*n);
 subplot(212);stem(n,y,'.');
