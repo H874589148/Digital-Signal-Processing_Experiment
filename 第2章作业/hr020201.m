4f5299f41121ef2df446e969c1695f8b7abcfdef
Hawkeye <40380233+H874589148@users.noreply.github.com>
2020-03-06 20:01:49 +0800

    加列以寻找matlab计算规律

--- 第2章作业/hr020201.m
+++ 第2章作业/hr020201.m
@@ -1,11 +1,20 @@
 clear;
 den = [1 -0.5 -0.005 0.3];   %设定函数的分母向量
 num=[1 2 1];         %设定函数的分子向量
-subplot(131);
+subplot(141);
 zplane(num,den);axis([-1.2 1.2 -1.2 1.2]);title('分子多项式[1 2 1]');%绘制系统函数的零极点图
+[Z P K]=tf2zp(num,den);
+fprintf('零点：\n');
+disp(Z);
+fprintf('极点：\n');
+disp(P);
+fprintf('增益： %.2f\n',K);
 num=[0 1 2 1];         %设定函数的分子向量
-subplot(132);
+subplot(142);
 zplane(num,den);axis([-1.2 1.2 -1.2 1.2]);title('分子多项式[0 1 2 1]');%绘制系统函数的零极点图
 num=[0 0 1 2 1];         %设定函数的分子向量
-subplot(133);
+subplot(143);
 zplane(num,den);axis([-1.2 1.2 -1.2 1.2]);title('分子多项式[0 0 1 2 1]');%绘制系统函数的零极点图
+num=[0 0 0 1 2 1];         %设定函数的分子向量
+subplot(144);
+zplane(num,den);axis([-1.2 1.2 -1.2 1.2]);title('分子多项式[0 0 0 1 2 1]');%绘制系统函数的零极点图
